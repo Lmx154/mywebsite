6ced61b9d32a35d9d79df6f5eb71cfffc1c6a13b
lmx154 <luis_mtz907@live.com>
2024-10-25 13:12:19 -0500

    resume container fixed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,45 +1,28 @@
 import React from "react";
 import "./About.css";
+import resume from "../assets/resume.pdf"; // Import the PDF file
 
 function About() {
   return (
     <div className="about">
-      <h1>About Me</h1>
-      <p>
-        Hi, I'm Luis Martinez. I am a software developer. I enjoy building web
-        applications and learning new technologies.
-      </p>
-      <p>My skills include:</p>
-      <ul>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>Node.js</li>
-        <li>CSS</li>
-        <li>C++</li>
-        <li>C</li>
-        <li>Python</li>
-        <li>Django</li>
-        <li>Django rest framework</li>
-        <li>Postgres SQL</li>
-        <li>MySQL</li>
-        <li>Ruby</li>
-        <li>Ruby on Rails</li>
-        {/* Add more skills as needed */}
-      </ul>
-      <p>
-        Below is my resume. You can also{" "}
-        <a href="/src/assets/resume.pdf" download>
-          download it here
-        </a>
-        .
-      </p>
       <div className="resume-container">
-        <iframe
-          src="/src/assets/resume.pdf"
-          width="100%"
-          height="600px"
-          title="Resume"
-        ></iframe>
+        <div className="responsive-iframe">
+          <iframe src={resume} title="Resume"></iframe>
+        </div>
+      </div>
+      <div className="about-content">
+        <h1>About Me</h1>
+        <p>
+          Hi, I&apos;m Luis Martinez. I am a software developer. I enjoy
+          building web applications and learning new technologies.
+        </p>
+        <p>
+          Below is my resume. You can also{" "}
+          <a href={resume} download>
+            download it here
+          </a>
+          .
+        </p>
       </div>
     </div>
   );
